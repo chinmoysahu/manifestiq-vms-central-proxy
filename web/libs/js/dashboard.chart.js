36da3ef5d27ece3afa952bdb18595367607d70e1
yeshraj <yashrajkhirwar@gmail.com>
2025-03-12 22:40:13 +0530

    testing for my side bar

---
+++ web/libs/js/dashboard.chart.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var theWindow = $('#tab-chart')
+    addOnTabOpen('chart', function () {
+
+    })
+    addOnTabReopen('chart', function () {
+
+    })
+    addToEventHandler('onDashboardReady', function(){
+
+    })
+})
