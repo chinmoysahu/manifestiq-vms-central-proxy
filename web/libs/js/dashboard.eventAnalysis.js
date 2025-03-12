a51ffb2414d4b92ff7b51ea08212e96afea7c42a
Shivam <mail2shivamchanddra@gmail.com>
2025-03-13 00:32:29 +0530

    Debug

--- web/libs/js/dashboard.eventAnalysis.js
+++ web/libs/js/dashboard.eventAnalysis.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
-    // Add Event Analysis Tab to the Sidebar
-    var sidebarMenu = $('#left-menu');
-    sidebarMenu.append(`
-        <li id="nav-event-analysis">
-            <a href="#" onclick="loadEventAnalysisTab()">
-                <i class="fa fa-chart-bar"></i> <span>Event Analysis</span>
-            </a>
-        </li>
-    `);
-
-    // Function to Load the Event Analysis Tab
-    window.loadEventAnalysisTab = function () {
-        $.get('/event-analysis', function (html) {
-            $('#tab-content').html(html);
+    // Function to Load Event Analysis Data
+    function loadEventAnalysisData() {
+        $.get('/api/event-analysis', function (data) {
+            let tableBody = $('#event-analysis-table tbody');
+            tableBody.empty();
+            
+            data.forEach(region => {
+                tableBody.append(`
+                    <tr>
+                        <td>${region.name}</td>
+                        <td>${region.openAlerts.toLocaleString()}</td>
+                        <td>${region.totalAlerts.toLocaleString()}</td>
+                    </tr>
+                `);
+            });
+        }).fail(function () {
+            console.error('Failed to load event analysis data');
         });
-    };
+    }
+
+    // Load Data on Tab Click
+    $(document).on('click', '#nav-event-analysis', function () {
+        loadEventAnalysisData();
+    });
+
+    // Initial Load
+    if ($('#event-analysis-table').length) {
+        loadEventAnalysisData();
+    }
 });
